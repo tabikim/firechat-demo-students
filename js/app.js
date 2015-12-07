039ef8a5ef488795bb0c4ad5094c54a5c5cd4acf
Tabitha Kim <tabijkim@gmail.com>
2015-12-07 06:51:49 -0800

    created repo for classmates to fork and clone for exercise

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// Initiate Angular app that includes firebase
+
+// Initiate controller for your app including scope, firebaseAuth, firebaseArray, firebaseObject
+
+// Create variable reference that refers to your firebase
+
+// Create an authObj that refers to your ref
+
+// Create authentication and create a new instance of firechat
+
+// Upon successful authentication, initiate firechat 
